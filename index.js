b9c1ec742ff22d2f6b76735b46d93e5ddf22d5f1
Mohamed Gueye <mohamed.gueye88@gmail.com>
2024-05-13 10:26:28 -0400

    Full project done

---
+++ index.js
@@ -0,0 +1,44 @@
+// Importing required modules
+import express from "express"; // Importing Express framework
+import bodyParser from "body-parser"; // Middleware to parse request bodies
+import axios from "axios"; // HTTP client for making requests
+
+// Initializing Express app
+const app = express();
+const port = 3000; // Port number on which the server will listen
+
+const API_URL = "https://api.coinbase.com/v2/prices/"; // Base URL for Coinbase API
+
+// Serving static files from 'public' directory
+app.use(express.static("public"));
+
+// Parsing incoming request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Handling GET request to the root URL
+app.get("/", (req, res) => {
+    // Rendering the 'index.ejs' template
+    res.render("index.ejs");
+});
+
+// Handling POST request to the '/submit' endpoint
+app.post("/submit", async (req, res) => {
+    try {
+        // Extracting the cryptocurrency code from the request body
+        const crypto = req.body.crypto;
+        // Sending a GET request to Coinbase API to fetch the price of the specified cryptocurrency
+        const result = await axios.get(API_URL + crypto + "-USD/buy");
+        // Extracting the price from the API response
+        const price = result.data.data.amount;
+        // Rendering the 'index.ejs' template with the price and cryptocurrency code
+        res.render("index.ejs", { content: price, crypto: crypto });
+    } catch (error) {
+        // Handling errors - rendering the 'index.ejs' template with an error message
+        res.render("index.ejs", { error: "Cryptocurrency code not found" });
+    } 
+});
+
+// Starting the server and listening on the specified port
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
